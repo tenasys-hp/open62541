8db5f5427a1f6fe6d2d1ba69ae8a7c2e911ac4f5
hlin <huipinglin@tenasys.onmicrosoft.com>
2021-02-03 08:24:38 +0100

    feat(arch): add global malloc singletons to INtime

--- arch/INtime/ua_architecture_functions.c
+++ arch/INtime/ua_architecture_functions.c
@@ -3,6 +3,14 @@
 #include <open62541/types.h>
 #include <sys/ioctl.h>
 
+/* Global malloc singletons */
+#ifdef UA_ENABLE_MALLOC_SINGLETON
+void *(*UA_mallocSingleton)(size_t size) = malloc;
+void (*UA_freeSingleton)(void *ptr) = free;
+void *(*UA_callocSingleton)(size_t nelem, size_t elsize) = calloc;
+void *(*UA_reallocSingleton)(void *ptr, size_t size) = realloc;
+#endif
+
 inline unsigned int
 UA_socket_set_blocking(UA_SOCKET sockfd) {
   u_long iMode = 0;
